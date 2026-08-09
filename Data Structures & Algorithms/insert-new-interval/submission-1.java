dfdacf356833b78f6d9ce8d526f5e58d6e8e3deb
Kaiyuan Zhao <31750664+kaiyuan588@users.noreply.github.com>
2026-08-09 14:57:17 -0400

    Add: insert-new-interval - submission-1

---
+++ Data Structures & Algorithms/insert-new-interval/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        
+        List<int[]> res = new ArrayList<>();
+        boolean inserted = false;
+        for (int i = 0; i < intervals.length; i++) {
+            int[] cur = intervals[i]; 
+            // if cur interval is on the left of the newInterval
+            if (cur[1] < newInterval[0]) {
+                res.add(cur);
+            } else if (cur[1] >= newInterval[0] && newInterval[1] >= cur[0]) {
+                newInterval = new int[]{Math.min(cur[0], newInterval[0]), Math.max(cur[1], newInterval[1])};
+            } else if (newInterval[1] < cur[0]) {
+                if (!inserted) {
+                    res.add(newInterval);
+                    inserted = true;
+                }
+                res.add(cur);
+            }
+            // if the cur interval and new Interval has overlap
+            // [a,b] [c,d]
+            // [c,d] [a,b]
+            // not overlap b < c || d < a (b >= c && d >= a)
+
+            // the the cur interval is on the right of the newInterval
+
+        }
+
+        if (!inserted) {
+            res.add(newInterval);
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
