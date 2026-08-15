7de6f4ec2ed5346a15f30a6f70d62a3576d29888
Kaiyuan Zhao <31750664+kaiyuan588@users.noreply.github.com>
2026-08-15 13:17:30 -0400

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dic = new HashSet<>();
+        for (String str: wordDict) {
+            dic.add(str);
+        }
+        int n = s.length();
+        boolean[] dp = new boolean[n+1]; // whether the first i characters of s can be segmented
+        dp[0] = true;
+
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (dp[j] && dic.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
